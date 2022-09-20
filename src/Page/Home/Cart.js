c3c247610db2b67e4ffa29e2c02e4b8790592ef6
mdrayhanislam <96991135+mdrayhanislam@users.noreply.github.com>
2022-09-20 11:47:11 +0600

    culculation

--- src/Page/Home/Cart.js
+++ src/Page/Home/Cart.js
@@ -1,10 +1,26 @@
 import React from 'react';
-
-const cart = ({cart}) => {
+const cart = ( props ) => {
+    const { cart } = props;
+    console.log(cart);
+    let total = 0;
+    let shipping = 0;
+    let quantity = 0;
+    for (const product of cart) {
+        quantity = quantity + product.quantity;
+        total = total + product.price
+        //  * product.quantity;
+        shipping = shipping + product.shipping;
+    }
+    const tax = parseFloat((total * 0.1).toFixed(2));
+    const grandTotal = total + shipping + tax;
     return (
-        <div className="card-body sticky top-0">
+        <div className="mt-20 ml-20">
             <h4 className='text-2xl'>Order Summary</h4>
             <p>Select Items: {cart.length}</p>
+            <p>Total price: ${total}</p>
+            <p>Total Shipping: ${shipping}</p>
+            <p>Tax: {tax}</p>
+            <h5>Grand Total: {grandTotal.toFixed(2)}</h5>
         </div>
     );
 };
--- src/Page/Home/Shops.js
+++ src/Page/Home/Shops.js
@@ -12,13 +12,13 @@ const Shops = () => {
     }, [])
 
     const handleAddToCart = (Product) => {
-        console.log(Product);
+        // console.log(Product);
         const newCart = [...cart, Product];
         setCart(newCart)
     }
 
     return (
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-20'>
+        <div className='grid grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-20'>
             <div>
                 <h1 className="text-2xl">this product: {products.length} </h1>
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-20 px-10px'>
